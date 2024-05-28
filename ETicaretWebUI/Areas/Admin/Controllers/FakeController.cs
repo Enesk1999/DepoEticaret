db3ec48fcdb96524d699cd5bb7ab1bc6a695fd57
Enesk1999 <130606254+Enesk1999@users.noreply.github.com>
2024-05-28 20:17:23 +0300

    updateslast

---
+++ ETicaretWebUI/Areas/Admin/Controllers/FakeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretWebUI.Areas.Admin.Controllers
+{
+	public class FakeController : Controller
+	{
+		public IActionResult Index()
+		{
+			var ss = "sds";
+			var sss = "fakerepos";
+			return View();
+		}
+	}
+}
---
+++ ETicaretWebUI/Areas/Customer/Controllers/FakeByDeletedController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretWebUI.Areas.Customer.Controllers
+{
+	public class FakeByDeletedController : Controller
+	{
+		public IActionResult Index()
+		{
+			var las = "sasd";
+			return View();
+		}
+	}
+}
