218c00af24ee5ee77a7aef900e584c7fb689541b
Enesk1999 <130606254+Enesk1999@users.noreply.github.com>
2024-05-28 22:34:21 +0300

    ss

---
+++ ETicaret.Model/Models/Fakee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Model.Models
+{
+	internal class Fakee
+	{
+	}
+}
